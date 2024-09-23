0decaef2ce16d76172f1309d958211e1582036e2
Anandhu p <myselfanandhup@gmail.com>
2024-09-23 16:27:07 +0530

    App Bar content finish here

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,66 @@
-main() {}
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+        ),
+      ),
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          "My Application",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          color: Colors.white,
+          onPressed: () {},
+          icon: Icon(
+            Icons.account_box_outlined,
+          ),
+        ),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera,
+            ),
+          ),
+          IconButton(
+            color: Colors.white,
+            onPressed: () {},
+            icon: Icon(
+              Icons.more_vert,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
