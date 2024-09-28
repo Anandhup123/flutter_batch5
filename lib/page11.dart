212d643f8f83788619ca8f759e57ccd90fe71b5e
Anandhu p <myselfanandhup@gmail.com>
2024-09-28 09:19:21 +0530

    added a radioListtile example code for a quiz

--- lib/page11.dart
+++ lib/page11.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_batch5/page10.dart';
 
 class Screen12 extends StatefulWidget {
   const Screen12({super.key});
@@ -9,6 +10,7 @@ class Screen12 extends StatefulWidget {
 
 class _Screen12State extends State<Screen12> {
   String? gender;
+  String? ans;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +21,7 @@ class _Screen12State extends State<Screen12> {
         child: Center(
           child: ListView(
             children: [
-              Text(
+              const Text(
                 "Gender Selection",
                 style: TextStyle(
                   fontSize: 30,
@@ -74,6 +76,75 @@ class _Screen12State extends State<Screen12> {
                       content: Text("Your selected Gender is $gender"),
                     ),
                   );
+                },
+                child: Text("Submit"),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "find out odd one",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.indigo,
+                ),
+              ),
+              RadioListTile(
+                title: Text("18"),
+                value: "wrong1",
+                groupValue: ans,
+                onChanged: (String? value) {
+                  setState(() {
+                    ans = value;
+                  });
+                },
+              ),
+              RadioListTile(
+                title: Text("19"),
+                value: "correct",
+                groupValue: ans,
+                onChanged: (String? value) {
+                  setState(() {
+                    ans = value;
+                  });
+                },
+              ),
+              RadioListTile(
+                title: Text("20"),
+                value: "wrong2",
+                groupValue: ans,
+                onChanged: (String? value) {
+                  setState(() {
+                    ans = value;
+                  });
+                },
+              ),
+              RadioListTile(
+                title: Text("21"),
+                value: "wrong3",
+                groupValue: ans,
+                onChanged: (String? value) {
+                  setState(() {
+                    ans = value;
+                  });
+                },
+              ),
+              TextButton(
+                onPressed: () {
+                  if (ans == "correct") {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => Screen11(),
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      backgroundColor: Colors.red,
+                      behavior: SnackBarBehavior.floating,
+                      content: Text("Wrong Answer Try Again"),
+                    ));
+                  }
                 },
                 child: Text("Submit"),
               )
